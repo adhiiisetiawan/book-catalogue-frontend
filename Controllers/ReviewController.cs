1f520005fe9f8f10829b64e46edc0a9c6f79c98c
adhiiisetiawan <adhisetiawan518@gmail.com>
2020-03-18 13:05:40 +0700

    first

---
+++ Controllers/ReviewController.cs
@@ -0,0 +1,90 @@
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using BooksCatalogue.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BooksCatalogue.Controllers
+{
+    public class ReviewController : Controller
+    {
+        private string apiEndpoint = "https://berapakali.azurewebsites.net/api/";
+        private string apiEndpoint2 = "https://berapakali.azurewebsites.net/api/review/";
+        public string baseUrl = "https://katalog-buku.azurewebsites.net/Books/Details/";
+        public HttpClient _client;
+        HttpClientHandler clientHandler = new HttpClientHandler();
+        
+        public ReviewController() {
+            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            _client = new HttpClient(clientHandler);
+        }
+
+        // GET: Review/AddReview/2
+        public async Task<IActionResult> AddReview(int? bookId)
+        {
+            if (bookId == null)
+            {
+                return NotFound();
+            }
+
+            // HttpClient client = new HttpClient();
+            _client = new HttpClient(clientHandler);
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, apiEndpoint + "books/" + bookId);
+
+            HttpResponseMessage response = await _client.SendAsync(request);
+
+            switch(response.StatusCode)
+            {
+                case HttpStatusCode.OK:
+                    string responseString = await response.Content.ReadAsStringAsync();
+                    var book = JsonSerializer.Deserialize<Book>(responseString);
+
+                    ViewData["BookId"] = bookId;
+                    return View("Add");
+                case HttpStatusCode.NotFound:
+                    return NotFound();
+                default:
+                    return ErrorAction("Error. Status code = " + response.StatusCode + ": " + response.ReasonPhrase);
+            }
+        }
+
+        // TODO: Tambahkan fungsi ini untuk mengirimkan atau POST data review menuju API
+        // POST: Review/AddReview
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AddReview([Bind("Id,BookId,ReviewerName,Rating,Comment")] Review review)
+        {
+            _client = new HttpClient(clientHandler);
+            MultipartFormDataContent content = new MultipartFormDataContent();
+            
+            content.Add(new StringContent(review.BookId.ToString()), "bookId");
+            content.Add(new StringContent(review.ReviewerName), "reviewerName");
+            content.Add(new StringContent(review.Rating.ToString()), "rating");
+            content.Add(new StringContent(review.Comment), "comment");
+
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, apiEndpoint2);
+            request.Content = content;
+            HttpResponseMessage response = await _client.SendAsync(request);
+            
+            switch (response.StatusCode)
+            {
+                case HttpStatusCode.OK:
+                case HttpStatusCode.NoContent:
+                case HttpStatusCode.Created:
+                    int idbooks = review.BookId;
+                     return Redirect(baseUrl + idbooks);
+                    // return View("Views/Books/Details.cshtml");
+                default:
+                    return ErrorAction("Error. Status code = " + response.StatusCode + "; " + response.ReasonPhrase);
+            }
+            // return Revi(review);
+        }
+
+        private ActionResult ErrorAction(string message)
+        {
+            return new RedirectResult("/Home/Error?message=" + message);
+        }
+    }
+}
